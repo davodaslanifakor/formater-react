b3b730db6e9ea03f84a79d64aceeb7982fbbb104
davodaslanifakor <davodaslanifakor@gmail.com>
2018-08-12 19:24:14 +0430

    remove log

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-console.log("helllo     ");
-
 class App extends Component {
   render() {
     return (
