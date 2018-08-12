9633a8a0ffbdc00f3fc7578f54ad8203cb0bb0f1
davodaslanifakor <davodaslanifakor@gmail.com>
2018-08-12 19:05:03 +0430

    test 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-console.log("helllo     "     )   ;
+console.log  ("helllo     "     )     ;
 
 class App extends Component {
   render() {
